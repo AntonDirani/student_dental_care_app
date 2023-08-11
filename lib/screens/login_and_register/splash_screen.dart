41104989d827a0ceb99b43ba95414273d0529a9e
Anton Dirani <antondirani@outlook.com>
2023-08-11 16:03:04 +0300

    Ready

--- lib/screens/login_and_register/splash_screen.dart
+++ lib/screens/login_and_register/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:student_care_app/controllers/location_controller.dart';
+import 'package:student_care_app/screens/login_and_register/do_you_have_an_account.dart';
 
 import '../../controllers/university_controller.dart';
 import '../../home_screen.dart';
@@ -53,7 +54,7 @@ class SplashScreen extends StatelessWidget {
                             Navigator.pushReplacement(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => HomeScreen()),
+                                  builder: (context) => DoYouHaveAnAccount()),
                             );
                           },
                           text: AppStrings.continueText,
